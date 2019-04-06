05c722cf32974119c48d1bce80888408037570ee
Damian Sztuczka <gardanw@gmail.com>
2019-04-06 12:59:59 +0000

    Delete L_j.py

--- L_j.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-from Potencjal import Potencjal
-
-class L_j(Potencjal):
-    def __init__(self, eps, r0):
-        self.__eps = eps
-        self.__r0 = r0
-    
