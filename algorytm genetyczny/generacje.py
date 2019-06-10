1776d1bb6dff37887ab5484f57b3adb7e9dfdb73
Damian Sztuczka <gardanw@gmail.com>
2019-06-10 19:35:15 +0200

    liczenie scora

--- algorytm genetyczny/generacje.py
+++ algorytm genetyczny/generacje.py
@@ -5,7 +5,23 @@ import numpy as np
 populacja = []
 for i in range(20):
     populacja.append(np.random.choice([0,1]))
-
+    
+wzor = []
+pom = []
+for i in range(len(populacja)):
+    if i%2 == 0:
+        pom.append(1)
+    else:
+        pom.append(0)
+wzor.append(pom)
+pom = []
+for i in range(len(populacja)):
+    if i%2 != 0:
+        pom.append(1)
+    else:
+        pom.append(0)
+wzor.append(pom)
+print(wzor)
 reg = []
 while len(reg) != 8:
     pom = np.random.randint(0,255)
@@ -25,23 +41,54 @@ for i in range(len(reg)):
         slownik[stany[j]] = reg[i][j]
     reg[i] = slownik
 print(reg)
-populacja_nowa = populacja[:]
-print(populacja)
-#for i in range(len(reg)):
-for i in range(500):
-    for j in range(len(populacja_nowa)):
-        pom = []
-        if j < len(populacja_nowa) - 1:
-            pom.append(str(populacja_nowa[j-1]))
-            pom.append(str(populacja_nowa[j]))
-            pom.append(str(populacja_nowa[j+1]))
-            klucz = ''.join(pom)
-            populacja_nowa[j] = int(reg[0][klucz])
-        else:
-            pom.append(str(populacja_nowa[j-1]))
-            pom.append(str(populacja_nowa[j]))
-            pom.append(str(populacja_nowa[0]))
-            klucz = ''.join(pom)
-            populacja_nowa[j] = int(reg[0][klucz])
-    print(populacja_nowa)
+
+populacje_po = []
+for i in range(len(reg)):
+    populacja_nowa = populacja[:]
+#    print(populacja)
+    for step in range(500):
+        for j in range(len(populacja_nowa)):
+            pom = []
+            if j < len(populacja_nowa) - 1:
+                pom.append(str(populacja_nowa[j-1]))
+                pom.append(str(populacja_nowa[j]))
+                pom.append(str(populacja_nowa[j+1]))
+                klucz = ''.join(pom)
+                populacja_nowa[j] = int(reg[i][klucz])
+            else:
+                pom.append(str(populacja_nowa[j-1]))
+                pom.append(str(populacja_nowa[j]))
+                pom.append(str(populacja_nowa[0]))
+                klucz = ''.join(pom)
+                populacja_nowa[j] = int(reg[i][klucz])
+#        print(populacja_nowa)
+    reg[i]['populacja_po'] = populacja_nowa
+    populacje_po.append(populacja_nowa)
+for i in populacje_po:
+    print(i)
+
+for i in range(len(reg)):
+    score = []
+    
+    for w in range(len(wzor)):
+        s = 0
+        for j in range(len(reg[i]['populacja_po'])):    
+            if reg[i]['populacja_po'][j] == wzor[w][j]:
+                s += 1
+        score.append(s)
+    print(score)
+    reg[i]['score'] = max(score)
+    
+for i in reg:
+    print(i)
+score = []
+for i in reg:
+    score.append(i['score'])
+print(score)
+while len(reg) > 4:
+    reg.pop(score.index(min(score)))
+    score.pop(score.index(min(score)))
+
+for i in reg:
+    print(i)
 
