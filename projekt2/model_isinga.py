1c4381518330a3f477f1922b0ad6e5c9bbc55f33
Damian Sztuczka <gardanw@gmail.com>
2019-06-11 21:26:48 +0200

    rysuje wykres ale zle

--- projekt2/model_isinga.py
+++ projekt2/model_isinga.py
@@ -3,6 +3,7 @@
 import random as ran
 import numpy as np
 import matplotlib.pyplot as plt
+import copy
 
 def energy(siatka):
     n = len(siatka[0])
@@ -17,30 +18,32 @@ def energy(siatka):
     return e
 
 n = 10
-Tem = np.linspace(0.1, 20, 200)
+Tem = np.linspace(0.1, 20, 100)
 e_mean = np.zeros(len(Tem))
 e_std = np.zeros(len(Tem))
+
+
+s = np.zeros([n, n])
+for x in range(n):
+    for y in range(n):
+        s[x][y] = ran.choice([-1, 1])
+    
+for i in s:
+    print(i)
 for step in range(len(Tem)):
-    T = Tem[step]
     print('step = ', step)
-    siatka = np.zeros([n, n])
-    for x in range(n):
-        for y in range(n):
-            siatka[x][y] = ran.choice([-1, 1])
-    
-    for i in siatka:
-        print(i)
-    
+    T = Tem[step]
+    siatka = copy.deepcopy(s)
     H = energy(siatka)
 
     suma = 0
     for i in range(len(H)):
         suma += sum(H[i])
     print(suma)
-    steps = 1000
+    steps = len(siatka)**2
     energie = np.zeros(steps)
     for k in range(steps):
-        siatka_new = siatka[:]
+        siatka_new = copy.deepcopy(siatka)
         x = ran.randrange(len(siatka))
         y = ran.randrange(len(siatka[0]))
         siatka_new[x][y] = siatka_new[x][y]*(-1)
@@ -49,15 +52,15 @@ for step in range(len(Tem)):
         for i in range(len(H_new)):
             suma_new += sum(H_new[i])
         if suma_new < suma:
-            H = H_new[:]
+            H = copy.deepcopy(H_new)
             suma = suma_new
-            siatka = siatka_new[:]
+            siatka = copy.deepcopy(siatka_new)
         else:
             prawdo = ran.random()
             if prawdo > np.exp(-(suma_new - suma)/T):
-                H = H_new[:]
+                H = copy.deepcopy(H_new)
                 suma = suma_new
-                siatka = siatka_new[:]
+                siatka = copy.deepcopy(siatka_new)
         energie[k] = suma
     e_mean[step] = np.mean(energie)
     e_std[step] = np.std(energie)
